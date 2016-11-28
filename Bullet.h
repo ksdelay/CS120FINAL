754ce73293b62d8a1c00c85ce58ba42974755966
junchili <jerryjunchili@gmail.com>
2016-11-27 21:08:20 -0500

    player and bullet
    add  the RMEs

--- Bullet.h
+++ Bullet.h
@@ -10,32 +10,129 @@ using namespace std;
 
 class Bullet {
 public:
+	/**
+	* Default Constructor
+	* Requires: nothing
+	* Modifies: xLocation,yLocation,speed,size,direction
+	* Effects: sets default Bullet class value
+	* Calls: nothing
+	*/
 	Bullet();
-
+	/**
+	* non-Default Constructor
+	* Requires: values for x and y location
+	* Modifies: xLocation,yLocation,speed,size,direction
+	* Effects: sets bullet x and y location Bullet class value
+	* Calls: nothing
+	*/
 	Bullet(double xLocation, double yLocation);
 
+	/**
+	* XLocation Getter
+	* Requires: nothing
+	* Modifies: nothing
+	* Effects: returns the value of x location
+	* Calls: nothing
+	*/
 	double getXLocation() const;
 
+	/**
+	* YLocation Getter
+	* Requires: nothing
+	* Modifies: nothing
+	* Effects: returns the value of y location
+	* Calls: nothing
+	*/
 	double getYLocation() const;
 
+	/**
+	* speed Getter
+	* Requires: nothing
+	* Modifies: nothing
+	* Effects: returns the value of speed
+	* Calls: nothing
+	*/
 	double getSpeed() const;
 
+	/**
+	* size Getter
+	* Requires: nothing
+	* Modifies: nothing
+	* Effects: returns the value of size
+	* Calls: nothing
+	*/
 	double getSize() const;
 
+	/**
+	* Direction Getter
+	* Requires: nothing
+	* Modifies: nothing
+	* Effects: returns the value of direction
+	* Calls: nothing
+	*/
 	Direction getDirection() const;
 
+	/**
+	* speed Setter
+	* Requires: nothing
+	* Modifies: speed
+	* Effects: set the speed
+	* Calls: nothing
+	*/
 	void setSpeed(double speed);
 
+	/**
+	* size Setter
+	* Requires: nothing
+	* Modifies: size
+	* Effects: set the size
+	* Calls: nothing
+	*/
 	void setSize(double size);
 
+	/**
+	* direction Setter
+	* Requires: nothing
+	* Modifies: direction
+	* Effects: set the direction
+	* Calls: Direction::setDirection()
+	*/
 	void setDirection(Direction direction);
 
+	/**
+	* XLocation Setter
+	* Requires: nothing
+	* Modifies: xLocation
+	* Effects: set the x location
+	* Calls: nothing
+	*/
 	void setXLocation(double xLocation);
 
+	/**
+	* YLocation Setter
+	* Requires: nothing
+	* Modifies: yLocation
+	* Effects: set the y location
+	* Calls: nothing
+	*/
 	void setYLocation(double yLocation);
 
+	/**
+	* Move Bullet
+	* Requires: nothing
+	* Modifies: size, xLocation, yLocation
+	* Effects: let bullet move by speed and direction
+	* Calls: nothing
+	*/
 	void BulletMove();
 
+	/**
+	* Bullet hit
+	* Requires: nothing
+	* Modifies: size
+	* Effects: make bullet size to 0 when bullet hit
+	* Calls: nothing
+	*/
 	void BulletHit();
 
 private:
--- Player.h
+++ Player.h
@@ -11,40 +11,164 @@ using namespace std;
 class Player
 {
 public:
+	/**
+	* Default Constructor
+	* Requires: nothing
+	* Modifies: points, userName,xLocation,yLocation,speed,direction
+	* Effects: sets default Player class value
+	* Calls: nothing
+	*/
 	Player();
-
+	/**
+	* non-Default Constructor
+	* Requires: userName, x y location.
+	* Modifies: points, userName,xLocation,yLocation,speed,direction
+	* Effects: sets userName and x y location
+	* Calls: nothing
+	*/
     Player(string userName, double xLocation, double yLocation);
 
+	/**
+	* userName Getter
+	* Requires: nothing
+	* Modifies: nothing
+	* Effects: returns the value of userName
+	* Calls: nothing
+	*/
     string getUserName();
 
+	/**
+	* userName Setter
+	* Requires: nothing
+	* Modifies: userName
+	* Effects: set the userName
+	* Calls: nothing
+	*/
 	void setUserName(string userName);
 
+	/**
+	* points Getter
+	* Requires: nothing
+	* Modifies: nothing
+	* Effects: returns the value of points
+	* Calls: nothing
+	*/
     int returnPoints();
 
+	/**
+	* points Setter
+	* Requires: nothing
+	* Modifies: points
+	* Effects: set the points
+	* Calls: nothing
+	*/
 	void setPoints(int points);
 
+	/**
+	* XLocation Getter
+	* Requires: nothing
+	* Modifies: nothing
+	* Effects: returns the value of x location
+	* Calls: nothing
+	*/
 	double getXLocation() const;
 
+	/**
+	* YLocation Getter
+	* Requires: nothing
+	* Modifies: nothing
+	* Effects: returns the value of y location
+	* Calls: nothing
+	*/
 	double getYLocation() const;
-
+	/**
+	* Direction Getter
+	* Requires: nothing
+	* Modifies: nothing
+	* Effects: returns the value of direction
+	* Calls: nothing
+	*/
 	Direction getDirection() const;
 
+	/**
+	* XLocation Setter
+	* Requires: nothing
+	* Modifies: xLocation
+	* Effects: set the x location
+	* Calls: nothing
+	*/
 	void setXLocation(double xLocation);
 
+	/**
+	* YLocation Setter
+	* Requires: nothing
+	* Modifies: yLocation
+	* Effects: set the y location
+	* Calls: nothing
+	*/
 	void setYLocation(double yLocation);
 
+	/**
+	* direction Setter
+	* Requires: nothing
+	* Modifies: direction
+	* Effects: set the direction
+	* Calls: Direction::setDirection()
+	*/
 	void setDirection(Direction direction);
 
+	/**
+	* shoot bullet
+	* Requires: nothing
+	* Modifies: shootBullet
+	* Effects: make the shoot bullet move
+	* Calls: Bullet::BulletMove,setSize,setYLocation
+	*/
     void playerShoot();
 
+	/**
+	* player move
+	* Requires: nothing
+	* Modifies: xLocation, yLocation
+	* Effects: make player move
+	* Calls: nothing
+	*/
     void playerMove();
 
+	/**
+	* Bullet Getter
+	* Requires: nothing
+	* Modifies: nothing
+	* Effects: get the bullet
+	* Calls: nothing
+	*/
 	Bullet getBullet() const;
 
+	/**
+	* Bullet Setter
+	* Requires: nothing
+	* Modifies: bullet
+	* Effects: set the bullet
+	* Calls: Bullet::Bullet()
+	*/
 	void setBullet(Bullet bullet);
 
+	/**
+	* shootBullet Getter
+	* Requires: nothing
+	* Modifies: nothing
+	* Effects: get the shootBullet
+	* Calls: nothing
+	*/
 	Bullet getShootBullet() const;
 
+	/**
+	* shootBullet Setter
+	* Requires: nothing
+	* Modifies: shootBullet
+	* Effects: set the shootBullet
+	* Calls: Bullet::Bullet()
+	*/
 	void setShootBullet(Bullet bullet);
 private:
 
